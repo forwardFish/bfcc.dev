bbcd2b351a5d4f3f3da1b1e16f1edbaa04de0a06
BFChain <developer@bfchain.org>
2021-02-02 10:36:54 +0800

    merged

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,64 +1,63 @@
 module.exports = {
-  '/01.guide/pc/': [
-    { text: '教程简介', link: '/01.guide/pc/' },
-    { text: '安装运行', link: '/01.guide/pc/01.install.html' },
-    { text: '节点部署', link: '/01.guide/pc/02.deploy.html' },
-    { text: '常见问题', link: '/01.guide/pc/03.faq.html' },
-    { text: '名词解释', link: '/01.guide/pc/04.glossary.html' }
+  '/guide/pc/': [
+    { text: '教程简介', link: '/guide/pc/' },
+    { text: '安装运行', link: '/guide/pc/install' },
+    { text: '节点部署', link: '/guide/pc/deploy' },
+    { text: '常见问题', link: '/guide/pc/faq' },
+    { text: '名词解释', link: '/guide/pc/glossary' }
   ],
-  '/02.api/pc/': [
+  '/api/pc/': [
     {
       text: '接口列表',
       children: [
-        { text: '概述', link: '/02.api/pc/1.0.html' },
-        { text: '接口传入参数和返回参数说明', link: '/02.api/pc/1.1.html' },
-        { text: '基础接口使用说明', link: '/02.api/pc/1.2.html' },
-        { text: '发送事件接口使用说明', link: '/02.api/pc/1.3.html' },
-        { text: '节点管理接口使用说明', link: '/02.api/pc/1.4.html' },
-        { text: '类型定义', link: '/02.api/pc/1.5.html' }
+        { text: '概述', link: '/api/pc/1-0' },
+        { text: '接口传入参数和返回参数说明', link: '/api/pc/1-1' },
+        { text: '基础接口使用说明', link: '/api/pc/1-2' },
+        { text: '发送事件接口使用说明', link: '/api/pc/1-3' },
+        { text: '节点管理接口使用说明', link: '/api/pc/1-4' },
+        { text: '类型定义', link: '/api/pc/1-5' }
       ]
     },
     {
       text: '调用方式以及示例',
       children: [
-        { text: '命令行工具', link: '/02.api/pc/2.1.html' },
-        { text: 'Websocket', link: '/02.api/pc/2.2.html' },
-        { text: 'Grpc', link: '/02.api/pc/2.3.html' },
-        { text: 'SDK', link: '/02.api/pc/2.4.html' }
+        { text: '命令行工具', link: '/api/pc/2-1' },
+        { text: 'Websocket', link: '/api/pc/2-2' },
+        { text: 'Grpc', link: '/api/pc/2-3' },
+        { text: 'SDK', link: '/api/pc/2-4' }
       ]
     }
   ],
-  '/02.api/bfs/': [
+  '/api/bfs/': [
     {
       text: 'BFS系统API',
       children: [
-        { text: 'bfs', link: '/02.api/bfs/bfs' },
-        { text: 'evt', link: '/02.api/bfs/evt' },
+        { text: 'bfs', link: '/api/bfs/bfs' },
+        { text: 'evt', link: '/api/bfs/evt' },
         {
           text: 'bfsprocess',
-          link: '/02.api/bfs/bfsprocess'
+          link: '/api/bfs/bfsprocess'
         },
-        { text: 'link', link: '/02.api/bfs/link' },
-        { text: 'file_system', link: '/02.api/bfs/file_system' },
-        { text: 'openBrl', link: '/02.api/bfs/openBrl' },
-        { text: 'WebViewActivity', link: '/02.api/bfs/WebViewActivity' }
+        { text: 'link', link: '/api/bfs/link' },
+        { text: 'file_system', link: '/api/bfs/file_system' },
+        { text: 'openBrl', link: '/api/bfs/openBrl' },
+        { text: 'WebViewActivity', link: '/api/bfs/WebViewActivity' }
       ]
     },
     {
       text: 'BFS应用',
       children: [
-        { text: 'TUI', link: '/02.api/bfs/BTP'},
-        { text: 'BFChain', 
-          children: [
-            { text: 'borker', link: '/02.api/bfs/borker' },
-          ]
+        { text: 'TUI', link: '/api/bfs/BTP' },
+        {
+          text: 'BFChain',
+          children: [{ text: 'borker', link: '/api/bfs/borker' }]
         },
-        { text: 'FinChain'},
+        { text: 'FinChain' }
         // children: [
-        //   { text: 'BTP数据传输协议', link: '/02.api/bfs/BTP' },
-        //   { text: 'BTPTools', link: '/02.api/bfs/BTPTools' },
+        //   { text: 'BTP数据传输协议', link: '/api/bfs/BTP' },
+        //   { text: 'BTPTools', link: '/api/bfs/BTPTools' },
         // ]
-        // { text: 'TUI', link: '/02.api/bfs/DomActivity' },
+        // { text: 'TUI', link: '/api/bfs/DomActivity' },
       ]
     }
   ]
