753d1ff1ff7baf2f057ac9b6ce18df3dab5e3e87
BFChainer <420288518@qq.com>
2021-06-02 16:39:46 +0800

    :pencil: 完善webview升级流程

--- docs/.vitepress/theme/components/MyWebviewUpgrade.vue
+++ docs/.vitepress/theme/components/MyWebviewUpgrade.vue
@@ -3,11 +3,17 @@
     <div class="container">
       <h1 class="title">Android系统WebView版本升级</h1>
       <div class="content">
-        <h3>
-        请在手机浏览器打开以下地址，选择合适的版本下载升级
-        </h3>
+        <p>使用Android 系統打开应用时，屏幕显示空白，这可能是由于Android系統WebView的不兼容问题。 请按照如下步骤操作：</p>
         <p>
-          <a href="https://apkbog.com/en/apk/android-system-webview" target="_blank" rel="noopener noreferrer">https://apkbog.com/en/apk/android-system-webview</a>
+          <ol>
+            <li>请您在手机浏览器打开网址:
+            <a href="https://apkbog.com/en/apk/android-system-webview" target="_blank" rel="noopener noreferrer">https://apkbog.com/en/apk/android-system-webview</a>
+            </li>
+            <li>选择适合的WebView版本，建议70以上版本</li>
+            <li>下载WebView并安装</li>
+            <li>重新启动手机</li>
+            <li>重启手机后，打开应用，若屏幕依然显示为空白，请重新更换版本安装。</li>
+          </ol>
         </p>
       </div>
       
@@ -23,14 +29,12 @@
   justify-content:center;
   align-items:center;
   padding-top:12rem;
-  text-align:center;
 }
 .title{
   color: rgba(0,0,0,.7);
 }
 .content{
   margin-top: 3rem;
-  color: rgba(0,0,0,.4);
 }
 
 @media (min-width: 720px) {
