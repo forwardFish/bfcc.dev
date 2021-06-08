c682382950a4100906c314fbc34a53bac2a9cb3b
BFChainer <420288518@qq.com>
2021-06-08 19:03:06 +0800

    回退

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,8 @@ import DefaultTheme from 'vitepress/dist/client/theme-default'
 import MyHome from '/@theme/components/MyHome.vue'
 import MyBlog from '/@theme/components/MyBlog.vue'
 import MyWebviewUpgrade from '/@theme/components/MyWebviewUpgrade.vue'
+import IpList from '/@theme/components/IpList.vue'
+
 
 export default {
   ...DefaultTheme,
@@ -9,5 +11,6 @@ export default {
     app.component('MyHome', MyHome)
     app.component('MyBlog', MyBlog)
     app.component('MyWebviewUpgrade', MyWebviewUpgrade)
+    app.component('IpList',IpList)
   }
 }
