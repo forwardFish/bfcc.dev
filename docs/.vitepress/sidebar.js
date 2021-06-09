4047fc2ed675b28de379923ff00c512e0772cd72
BFChainer <420288518@qq.com>
2021-06-09 15:44:40 +0800

    :bug:修复侧边栏跳转偶尔报404的bug

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,30 +1,30 @@
 module.exports = {
   '/guide/pc/': [
-    { text: '教程简介', link: '/guide/pc/' },
-    { text: '安装运行', link: '/guide/pc/install' },
-    { text: '节点部署', link: '/guide/pc/deploy' },
-    { text: '常见问题', link: '/guide/pc/faq' },
-    { text: '名词解释', link: '/guide/pc/glossary' }
+    { text: '教程简介', link: '/guide/pc/index.html' },
+    { text: '安装运行', link: '/guide/pc/install.html' },
+    { text: '节点部署', link: '/guide/pc/deploy.html' },
+    { text: '常见问题', link: '/guide/pc/faq.html' },
+    { text: '名词解释', link: '/guide/pc/glossary.html' }
   ],
   '/api/pc/': [
     {
       text: '接口列表',
       children: [
-        { text: '概述', link: '/api/pc/1-0' },
-        { text: '接口传入参数和返回参数说明', link: '/api/pc/1-1' },
-        { text: '基础接口使用说明', link: '/api/pc/1-2' },
-        { text: '发送事件接口使用说明', link: '/api/pc/1-3' },
-        { text: '节点管理接口使用说明', link: '/api/pc/1-4' },
-        { text: '类型定义', link: '/api/pc/1-5' }
+        { text: '概述', link: '/api/pc/1-0.html' },
+        { text: '接口传入参数和返回参数说明', link: '/api/pc/1-1.html' },
+        { text: '基础接口使用说明', link: '/api/pc/1-2.html' },
+        { text: '发送事件接口使用说明', link: '/api/pc/1-3.html' },
+        { text: '节点管理接口使用说明', link: '/api/pc/1-4.html' },
+        { text: '类型定义', link: '/api/pc/1-5.html' }
       ]
     },
     {
       text: '调用方式以及示例',
       children: [
-        { text: '命令行工具', link: '/api/pc/2-1' },
-        { text: 'Websocket', link: '/api/pc/2-2' },
-        { text: 'Grpc', link: '/api/pc/2-3' },
-        { text: 'SDK', link: '/api/pc/2-4' }
+        { text: '命令行工具', link: '/api/pc/2-1.html' },
+        { text: 'Websocket', link: '/api/pc/2-2.html' },
+        { text: 'Grpc', link: '/api/pc/2-3.html' },
+        { text: 'SDK', link: '/api/pc/2-4.html' }
       ]
     }
   ],
@@ -32,22 +32,22 @@ module.exports = {
     {
       text: 'BFS系统API',
       children: [
-        { text: 'bfs', link: '/api/bfs/bfs' },
-        { text: 'evt', link: '/api/bfs/evt' },
+        { text: 'bfs', link: '/api/bfs/bfs.html' },
+        { text: 'evt', link: '/api/bfs/evt.html' },
         {
-          text: 'bfsprocess',
-          link: '/api/bfs/bfsprocess'
+          text: 'bfsprocess.html',
+          link: '/api/bfs/bfsprocess.html'
         },
-        { text: 'link', link: '/api/bfs/link' },
-        { text: 'file_system', link: '/api/bfs/file_system' },
-        { text: 'openBrl', link: '/api/bfs/openBrl' },
-        { text: 'WebViewActivity', link: '/api/bfs/WebViewActivity' }
+        { text: 'link', link: '/api/bfs/link.html' },
+        { text: 'file_system', link: '/api/bfs/file_system.html' },
+        { text: 'openBrl', link: '/api/bfs/openBrl.html' },
+        { text: 'WebViewActivity', link: '/api/bfs/WebViewActivity.html' }
       ]
     },
     {
       text: 'BFS应用',
       children: [
-        { text: 'TUI', link: '/api/bfs/BTP' }
+        { text: 'TUI', link: '/api/bfs/BTP.html' }
         // {
         //   text: 'BFChain',
         //   children: [{ text: 'borker', link: '/api/bfs/borker' }]
@@ -65,9 +65,9 @@ module.exports = {
     {
       text: '知识库',
       children: [
-        { text: '共识激励机制', link: '/knowledge/01' },
-        { text: '共识投票机制', link: '/knowledge/02' },
-        { text: '区块锻造者竞选', link: '/knowledge/03' }
+        { text: '共识激励机制', link: '/knowledge/01.html' },
+        { text: '共识投票机制', link: '/knowledge/02.html' },
+        { text: '区块锻造者竞选', link: '/knowledge/03.html' }
       ]
     },
   ]
