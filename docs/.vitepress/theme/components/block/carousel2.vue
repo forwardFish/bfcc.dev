4d0ae9c5f66f8bd96dc4c4af824149ce1f49a9ee
Implement Team <implement@bfchain.org>
2021-08-10 14:30:54 +0800

    ✨  finish banner link

--- docs/.vitepress/theme/components/block/carousel2.vue
+++ docs/.vitepress/theme/components/block/carousel2.vue
@@ -6,31 +6,51 @@
       :image="slide.img"
       :link="slide.href"
     >
-      <!-- <template #content>
+      <template #content>
         <div class="content">
           <h2>{{ slide.content }}</h2>
           <a :href="slide.href"
             >前往下载
-            <img :src="btnDownload" />
+            <img :src="downIcon" />
           </a>
         </div>
-      </template> -->
+      </template>
     </vueper-slide>
   </vueper-slides>
 </template>
 <script setup lang="ts">
 import { VueperSlides, VueperSlide } from 'vueperslides'
 import 'vueperslides/dist/vueperslides.css'
-// import btnDownload from '../imgs/btn-download.png'
+import downIcon from '../imgs/down-icon.png'
 
 const props = defineProps({
   slides: Array
 })
 </script>
 <style scoped>
-/* .content {
+.content {
   color: #fff;
   width: 1024px;
   margin: 0 auto;
-} */
+}
+.content h2 {
+  margin-top: 140px;
+  width: 290px;
+  line-height: 60px;
+  border: 0;
+  font-size: 36px;
+}
+.content a {
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
+  width: 130px;
+  height: 50px;
+  text-align: center;
+}
+.content img {
+  margin-left: 5px;
+}
 </style>
