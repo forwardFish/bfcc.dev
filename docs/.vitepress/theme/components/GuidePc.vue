e5c70987c596eba3f16bfd4f92b15489fe5d2f2c
EmmanuelAgui <420288518@qq.com>
2021-08-11 19:12:44 +0800

    PC全节点新增构建区块链步骤

--- docs/.vitepress/theme/components/GuidePc.vue
+++ docs/.vitepress/theme/components/GuidePc.vue
@@ -1,6 +1,27 @@
 <template>
   <block-carousel class="top-carsousel" :slides="slideItems" />
   <div class="content">
+    <div class="build-chain">
+      <div class="title-section">
+        <div class="title">构建一条区块链</div>
+        <div class="sub-title">步骤概览</div>
+      </div>
+      <div class="steps-section">
+        <div class="step build_chain_step" v-for="build_chain_step in build_chain_steps" :key="build_chain_step">
+          <div class="step-img">
+            <img :src="build_chain_step.img" :alt="'步骤'+build_chain_step.index">
+          </div>
+          <div class="step-name">
+            {{build_chain_step.name}}
+          </div>
+          <!-- <div class="step-name-en">
+              {{build_chain_step.en}}
+            </div> -->
+          <div class="step-desc">{{build_chain_step.desc}}</div>
+          <div class="step-index">0{{build_chain_step.index}}</div>
+        </div>
+      </div>
+    </div>
     <div class="run-node-pc">
       <div class="title-section">
         <div class="title">运行一个PC全节点</div>
@@ -101,6 +122,76 @@ const slideItems = [
 ]
 
 const IMG_BASE = '/.vitepress/theme/components/imgs';
+let t = `
+构建一条区块链
+步骤概览
+1.BCF节点安装
+通过BFChain开发者社区下载并安装对应操作系统的BCF节点软件
+2.创世块申请
+按照要求向指定邮箱发送您要构建的创世块申请
+3.节点配置修改
+申请成功后，修改节点创世块信息及节点网络环境等配置参数
+4.防火墙配置
+根据部署方式(公有链/私有链/联盟链)配置防火墙
+5.节点身份绑定
+节点启动后，需要为节点绑定一个身份地址（普通地址或受托人地址）
+6.节点运行
+节点身份绑定后，则节点成功运行，届时节点可进行区块同步、锻造等
+7.节点管理
+通过命令行窗口交互的方式管理节点
+`
+const build_chain_steps = [
+  {
+    index:1,
+    name:'BCF节点安装',
+    en:'SYSTEM',
+    desc:'通过BFChain开发者社区下载并安装对应操作系统的BCF节点软件',
+    img:`${IMG_BASE}/build_step_01.png`
+  },
+  {
+    index:2,
+    name:'创世块申请',
+    en:'INSTALL',
+    desc:'按照要求向指定邮箱发送您要构建的创世块申请',
+    img:`${IMG_BASE}/build_step_02.png`
+  },
+  {
+    index:3,
+    name:'节点配置修改',
+    en:'MODIFICATION',
+    desc:'申请成功后，修改节点创世块信息及节点网络环境等配置参数',
+    img:`${IMG_BASE}/build_step_03.png`
+  },
+  {
+    index:4,
+    name:'防火墙配置',
+    en:'NODE',
+    desc:'根据部署方式(公有链/私有链/联盟链)配置防火墙',
+    img:`${IMG_BASE}/build_step_04.png`
+  },
+  {
+    index:5,
+    name:'节点身份绑定',
+    en:'NODE',
+    desc:'节点启动后，需要为节点绑定一个身份地址（普通地址或受托人地址）',
+    img:`${IMG_BASE}/build_step_05.png`
+  },
+  {
+    index:6,
+    name:'节点运行',
+    en:'OPERATION',
+    desc:'节点身份绑定后，则节点成功运行，届时节点可进行区块同步、锻造等',
+    img:`${IMG_BASE}/build_step_06.png`
+  },
+  {
+    index:7,
+    name:'节点管理',
+    en:'MANAGEMENT',
+    desc:'通过命令行或者节点管理器(若加入BFChain网络)管理节点',
+    img:`${IMG_BASE}/build_step_07.png`
+  },
+]
+
 const steps = [
   {
     index:1,
@@ -252,6 +343,14 @@ const guides = [
   list-style: none;
   border-right: 1px solid #e6e6e6;
   padding: 16px;
+}
+
+.build_chain_step{
+  height: 290px;
+  overflow: hidden;
+}
+.build_chain_step .step-img{
+  position: absolute;
 }
 
 .step-name{
